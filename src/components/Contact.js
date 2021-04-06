a1304bc99d9b083d0e93bbc934e2c03e609bc1b6
gustaveH <ghabiman@gmail.com>
2021-04-06 14:17:48 -0400

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,20 +6,24 @@ import chat from '../images/card/chat.png';
 const Contact = () => {
   return (
     <section className='contact'>
-      <HeaderText headerLine='Get in touch' />
-      <div className='chat-picture'>
-        <img src={chat} alt='lets chat' />
-      </div>
-      <div className='contact-form'>
-        <form>
-          <label>
-            <input placeholder='Full Name' name='name' />
-          </label>
-          <textarea placeholder='How Can We help?' />
-          <div className='submit-button'>
-            <Button message='Send Message' type='submit' value='Submit' />
-          </div>
-        </form>
+      <div className='container'>
+        <div className='get-in-touch'>
+          <HeaderText headerLine='Get in touch' id='contact-text' />
+        </div>
+        <div className='chat-picture'>
+          <img src={chat} alt='lets chat' />
+        </div>
+        <div className='contact-form'>
+          <form>
+            <label>
+              <input placeholder='Full Name' name='name' />
+            </label>
+            <textarea placeholder='How Can We help?' />
+            <div className='submit-button'>
+              <Button message='Send Message' type='submit' value='Submit' />
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   );
