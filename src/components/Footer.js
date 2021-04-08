3ba47a1d6a85a3f56fc4c1e40c17922146cc96f0
gustaveH <ghabiman@gmail.com>
2021-04-08 14:19:16 -0400

    added links to footer x2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Instagram from '../images/instagram.png';
 import MixcloudFooter from '../images/mixcloud-footer.png';
 import SpotifyFooter from '../images/spotify-footer.png';
@@ -10,15 +10,15 @@ const Footer = () => {
       <p className='text-footer'>&copy;Denwize</p>
       <p className='text-footer'>Follow us:</p>
       <div className='footer-social'>
-        <Link to='https://open.spotify.com/user/vm01elqhvak78e24ds1bk5rhc'>
+        <a href='https://open.spotify.com/user/vm01elqhvak78e24ds1bk5rhc'>
           <img src={SpotifyFooter} alt='Spotify' />
-        </Link>
-        <Link to='https://www.instagram.com/iamdjziza/'>
+        </a>
+        <a href='https://www.instagram.com/iamdjziza/'>
           <img src={Instagram} alt='Spotify' />
-        </Link>
-        <Link to='https://www.mixcloud.com/djziza/'>
+        </a>
+        <a href='https://www.mixcloud.com/djziza/'>
           <img src={MixcloudFooter} alt='Spotify' />
-        </Link>
+        </a>
       </div>
     </div>
   );
