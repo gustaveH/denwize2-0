e0a762ec3ab2afe92a950563f054432467c10ee1
gustaveH <ghabiman@gmail.com>
2021-04-08 13:58:19 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,11 @@ const Footer = () => {
       <p className='text-footer'>Follow us:</p>
       <div className='footer-social'>
         <img src={Instagram} alt='IG' />
-        <img src={SpotifyFooter} alt='Spotify' />
+        <img
+          src={SpotifyFooter}
+          alt='Spotify'
+          href='https://open.spotify.com/user/vm01elqhvak78e24ds1bk5rhc'
+        />
         <img src={MixcloudFooter} alt='Mixcloud' />
       </div>
     </div>
